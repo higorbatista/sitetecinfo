b02c49dae142c1ec8155dddf7f2cf90dc92bb476
higorbatista <higor.pm13@gmail.com>
2018-09-06 21:19:20 -0300

    criando arquivo

---
+++ principal.js
@@ -0,0 +1,13 @@
+var banners=
+	["os melhores do brasil", "qualidade"]
+var bannerAtual = 0;
+
+function trocaBanner(){
+	bannerAtual = (bannerAtual + 1) %2;
+	document.querySelector('h2#mansagem').textContent =
+		banner[bannerAtual];
+
+
+}
+
+Setintevalt('trocaBanner')
